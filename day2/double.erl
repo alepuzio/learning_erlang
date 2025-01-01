a26d7f72e37b30c3edaa5f264457ca2e88b241be
Alessandro Puzielli <alessandro.puzielli@alepuzio.net>
2025-01-01 11:50:43 +0100

    forgotten file

--- day2/double.erl
+++ day2/double.erl
@@ -1,5 +1,13 @@
+%declare module double
 -module(double).
-%-export([double_all/1]).
+%make visible and callable functiona double_all
+%with 1 paramether
+-export([double_all/1]).
 
-%double_all([]) -> [];
-% double_all([First|Rest]) -> [First + First | double_all(Rest)] .
+%body
+%if the parameter is empty, then return empty list
+double_all([]) -> [];
+%otherwise, double the first element 
+% and append the result of the call of the function
+% on the other elements
+double_all([First|Rest]) -> [First + First | double_all(Rest)] .
